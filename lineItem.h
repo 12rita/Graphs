aa98b3320dc9e173ef427843d80685fa8931bd15
12rita <12rita43@gmail.com>
2016-12-15 23:34:16 +0300

    ver 3.0

--- lineItem.h
+++ lineItem.h
@@ -2,16 +2,16 @@
 #define LINEITEM_H
 #include <QGraphicsLineItem>
 #include <QPointF>
-#include "CircleItem.h"
+#include "circleItem.h"
 
-class lineItem: public QGraphicsLineItem
+class lineItem : public QGraphicsLineItem
 {
 public:
-    lineItem();
-    QGraphicsItem* ver1;
-    QGraphicsItem* ver2;
-    lineItem(QGraphicsItem* a, QGraphicsItem* b);
-    void redo();
-    ~lineItem();
+	lineItem();
+	QGraphicsItem* ver1;
+	QGraphicsItem* ver2;
+	lineItem(QGraphicsItem* a, QGraphicsItem* b);
+	void redo();
+	~lineItem();
 };
 #endif // LINEITEM_H
