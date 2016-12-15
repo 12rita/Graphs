8dc3d7b0356f7a39040d422758c1c7bd649c41bb
12rita <12rita43@gmail.com>
2016-12-15 23:33:44 +0300

    ver 3.0

--- lineitem.cpp
+++ lineitem.cpp
@@ -6,11 +6,11 @@
 
 lineItem::lineItem(QGraphicsItem* a, QGraphicsItem* b)
 {
-    ver1 = a;
-    ver2 = b;
+	ver1 = a;
+	ver2 = b;
 
-    this->setLine(a->x() + 10, a->y() + 10, b->x() + 10, b->y() + 10);
-    setFlag(ItemIsSelectable);
+	this->setLine(a->x() + 10, a->y() + 10, b->x() + 10, b->y() + 10);
+	setFlag(ItemIsSelectable);
 }
 
 lineItem::~lineItem()
@@ -19,5 +19,5 @@ lineItem::~lineItem()
 
 void lineItem::redo()
 {
-    this->setLine(ver1->x() + 10, ver1->y() + 10, ver2->x() + 10, ver2->y() + 10);
+	this->setLine(ver1->x() + 10, ver1->y() + 10, ver2->x() + 10, ver2->y() + 10);
 }
